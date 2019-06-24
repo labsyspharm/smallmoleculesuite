8fe1fdc0b739dc96a430cfd09866e3260794f39e
nteetor <nathanteetor@gmail.com>
2019-06-24 17:04:09 -0400

    chore(*): get shiny project setup

---
+++ scripts/scripts_r/shiny_apps/the-whole-thing/ui.R
@@ -0,0 +1,13 @@
+htmlTemplate(
+  "templates/home.html",
+  document_ = FALSE,
+  selectivity = htmlTemplate(
+    "templates/selectivity.html"
+  ),
+  similarity = htmlTemplate(
+    "templates/similarity.html"
+  ),
+  library = htmlTemplate(
+    "templates/library.html"
+  )
+)
