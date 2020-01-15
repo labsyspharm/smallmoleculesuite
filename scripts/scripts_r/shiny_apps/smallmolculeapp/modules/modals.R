f470ef90b77e2c8c9b2c9ff6b1204ba7dac2662d
nteetor <nathanteetor@gmail.com>
2020-01-15 11:10:28 -0500

    fix(homepage): add paper link to moa modal (#42)

--- scripts/scripts_r/shiny_apps/smallmolculeapp/modules/modals.R
+++ scripts/scripts_r/shiny_apps/smallmolculeapp/modules/modals.R
@@ -26,7 +26,12 @@ moaModal <- function() {
     header = h4("LSP-MoA Library"),
     size = "lg",
     div(
-      p("The LSP_MoA library is a collection of compounds in the public domain that were selected based on criteria described in Moret et al. (2019)"),
+      p("The LSP_MoA library is a collection of compounds in the public domain that were selected based on criteria described in",
+        tags$a(
+          "Moret et al. (2019)", 
+          href = "https://www.cell.com/cell-chemical-biology/fulltext/S2451-9456(19)30073-X",
+          target = "_blank")
+      ),
       img(src = "assets/img/lsp-moa-query-target.png") %>% 
         margin(l = "auto", r = "auto", b = 2),
       p("The library was developed with the aim to have a pair of inhibitors for each protein target in the liganded genome with the members of the pair structurally distinct but similar in selectivity for the intended target, along with all approved drugs or clinical candidates that show affinity for said particular target (Figure 1). Being structurally distinct has the advantage that the compounds are the most likely to have orthogonal target affinity spectra (TAS) and phenotypic fingerprint (PFP). To assess whether two compounds are similar in selectivity, we discretized the selectivity of compounds into four qualitative levels; most selective (MS), semi selective (SS), poly selective (PS) and unknown (UN)." ),
