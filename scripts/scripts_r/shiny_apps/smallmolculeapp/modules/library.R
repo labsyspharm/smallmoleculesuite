ff1ec5f85b102194e2d845425a9afbe2bed535fe
Clemens Hug <clemens.hug@gmail.com>
2020-02-25 16:25:52 -0500

    Fix library module to work with new data

--- scripts/scripts_r/shiny_apps/smallmolculeapp/modules/library.R
+++ scripts/scripts_r/shiny_apps/smallmolculeapp/modules/library.R
@@ -1,6 +1,6 @@
 libraryUI <- function(id) {
   ns <- NS(id)
-  
+
   columns(
     column(
       width = 4,
@@ -27,12 +27,12 @@ libraryUI <- function(id) {
                   inputId = ns("gene_list"),
                   label = NULL,
                   rows = 5
-                ) %>% 
+                ) %>%
                   margin(top = -3),
                 help = div(
-                  "This tool uses HUGO names. Please see", 
+                  "This tool uses HUGO names. Please see",
                   tags$a(
-                    target = "_blank", href = "https://genenames.org", 
+                    target = "_blank", href = "https://genenames.org",
                     "genenames.org"
                   ),
                   "for help."
@@ -40,9 +40,9 @@ libraryUI <- function(id) {
               ),
               formSubmit(
                 label = "Pre-defined gene sets"
-              ) %>% 
+              ) %>%
                 background("orange")
-            ) %>% 
+            ) %>%
               margin(bottom = 3),
             formGroup(
               label = "Example gene lists",
@@ -52,9 +52,9 @@ libraryUI <- function(id) {
                   choices = names(data_genes), # data/load.R
                   selected = "Dark_Kinome"
                 )
-                
+
                 sel$children[[1]]$attribs$placeholder <- "Dark_Kinome"
-                
+
                 sel
               },
               help = "Selecting a choice will populate the input above with an example list of genes."
@@ -70,12 +70,12 @@ libraryUI <- function(id) {
                   linkInput(
                     id = ns("gene_unknowns"),
                     label = shiny::icon("exclamation-circle")
-                  ) %>% 
+                  ) %>%
                     font(color = "orange")
-                ) %>% 
-                  display("flex") %>% 
-                  flex(justify = "between", align = "center") %>% 
-                  margin(top = 4, bottom = 4) %>% 
+                ) %>%
+                  display("flex") %>%
+                  flex(justify = "between", align = "center") %>%
+                  margin(top = 4, bottom = 4) %>%
                   font(size = "lg"),
                 formGroup(
                   label = tags$h6("Probes") %>% margin(b = 0),
@@ -85,7 +85,7 @@ libraryUI <- function(id) {
                     choices = c("Most selective", "Semi-selective", "Poly-selective", "Unknown"),
                     # values = c("best", "second", "non", "un"),
                     selected = "Most selective"
-                  ) %>% 
+                  ) %>%
                     active("orange"),
                   help = "Choose the selectivity levels for which you want chemical probes to be included in the library."
                 ),
@@ -95,9 +95,9 @@ libraryUI <- function(id) {
                     inline = TRUE,
                     id = ns("filter_phase"),
                     choices = c("Approved", "Phase III", "Phase II", "Phase I"),
-                    values = c("approved", "max_phase_3", "max_phase_2", "max_phase_1"),
-                    selected = "approved"
-                  ) %>% 
+                    values = c(4, 3, 2, 1),
+                    selected = 4
+                  ) %>%
                     active("orange"),
                   help = "Select compounds in clinical development to be added to the library."
                 ),
@@ -108,7 +108,7 @@ libraryUI <- function(id) {
                     id = ns("filter_expert"),
                     choices = "chemicalprobes.org 4.0 star rating",
                     values = "chem_probe"
-                  ) %>% 
+                  ) %>%
                     active("orange"),
                   help = "Select compounds that are endorsed by other users to be added to the library."
                 ),
@@ -119,7 +119,7 @@ libraryUI <- function(id) {
                     choices = c("Display per entry", "Display per compound"),
                     values = c("entry", "compound"),
                     selected = "entry"
-                  ) %>% 
+                  ) %>%
                     active("orange")
                 ),
                 formGroup(
@@ -127,10 +127,10 @@ libraryUI <- function(id) {
                   div(
                     class = "logify-slider active--orange",
                     shiny::sliderInput(
-                      inputId = ns("filter_affinity"), 
+                      inputId = ns("filter_affinity"),
                       label = NULL,
                       min = 0,
-                      max = 5, 
+                      max = 5,
                       step = 0.25,
                       value = 3
                     )
@@ -141,24 +141,10 @@ libraryUI <- function(id) {
                   div(
                     class = "active--orange",
                     shiny::sliderInput(
-                      inputId = ns("filter_measurement"), 
+                      inputId = ns("filter_measurement"),
                       label = NULL,
-                      min = 1, 
+                      min = 1,
                       max = 40,
-                      value = 2
-                    )
-                  )
-                ),
-                formGroup(
-                  label = tags$h6("Maximum std. dev. of Kd (nM)") %>% margin(b = 0),
-                  div(
-                    class = "logify-slider active--orange",
-                    shiny::sliderInput(
-                      inputId = ns("filter_sd"), 
-                      label = NULL,
-                      min = 0, 
-                      max = 5, 
-                      step = 0.25,
                       value = 2
                     )
                   )
@@ -205,14 +191,14 @@ libraryUI <- function(id) {
             height = "625px"
           )
         )
-      ) %>% 
+      ) %>%
         margin(b = 3),
       card(
         h3("Reference cards"),
         htmlOutput(
           outputId = ns("chembl_list")
         )
-      ) %>% 
+      ) %>%
         margin(b = 2)
     )
   )
@@ -220,10 +206,10 @@ libraryUI <- function(id) {
 
 libraryServer <- function(input, output, session, load_example) {
   ns <- session$ns
-  
+
   # Define genes found in our data
-  data_all_genes <- dplyr::union(data_selection_clinical$symbol, data_selection_selectivity$symbol)
-  
+  data_all_genes <- data_selection_selectivity$symbol
+
   # nav ----
   observeEvent(input$nav, {
     switch(
@@ -232,42 +218,42 @@ libraryServer <- function(input, output, session, load_example) {
       instructions = showNavPane(ns("pane_instructions"))
     )
   })
-  
+
   # Load an example gene list
   observeEvent(input$gene_example, {
     shiny::updateTextAreaInput(
-      session = session, 
-      inputId = "gene_list", 
+      session = session,
+      inputId = "gene_list",
       value = paste0(data_genes[[input$gene_example]], collapse = "\n")
     )
   })
-  
+
   observeEvent(load_example(), once = TRUE, {
     session$sendCustomMessage("click.library.sm", list())
   })
-  
+
   observeEvent(input$gene_form, {
     showNavPane(ns("pane_results"))
   })
-  
+
   r_gene_list <- reactive({
     if (is.null(input$gene_list)) {
       return(NULL)
     }
     strsplit(input$gene_list, "\n")[[1]]
   })
-  
+
   r_gene_unknown <- reactive({
     dplyr::setdiff(r_gene_list(), data_all_genes)
   })
-  
+
   r_gene_known <- reactive({
     dplyr::intersect(data_all_genes, r_gene_list())
   })
-  
+
   observeEvent(input$gene_unknowns, {
     req(length(r_gene_unknown()) > 0)
-    
+
     showModal(
       modal(
         id = NULL,
@@ -277,7 +263,7 @@ libraryServer <- function(input, output, session, load_example) {
       )
     )
   })
-  
+
   output$gene_targets <- renderText({
     if (is.null(r_gene_list()) || length(r_gene_list()) < 1) {
       "No genes upload yet"
@@ -285,164 +271,163 @@ libraryServer <- function(input, output, session, load_example) {
       paste(length(r_gene_known()), "target(s) with at least one ligand")
     }
   })
-  
+
   r_selection_selectivity <- reactive({
     req(r_gene_known())
-    
+
     gene_select <- data_selection_selectivity %>%
-      dplyr::filter(symbol %in% r_gene_known()) 
-    
+      dplyr::filter(symbol %in% r_gene_known())
+
     if (is.null(input$filter_probes)) {
       return(dplyr::slice(gene_select, 0))
     }
-    
-    gene_select %>% 
-      dplyr::filter(source %in% input$filter_probes)
+
+    gene_select %>%
+      dplyr::filter(selectivity_class %in% input$filter_probes)
   })
-  
+
   r_selection_clinical <- reactive({
     req(r_gene_known())
-    
-    gene_clinical <- data_selection_clinical %>%
+
+    gene_clinical <- data_selection_selectivity %>%
       dplyr::filter(symbol %in% r_gene_known())
-    
+
     if (is.null(input$filter_phase)) {
       return(dplyr::slice(gene_clinical, 0))
     }
-    
-    phase_clinical <- gene_clinical %>% 
-      dplyr::filter(source %in% input$filter_phase)
-    
+
+    phase_clinical <- gene_clinical %>%
+      dplyr::filter(max_phase %in% input$filter_phase)
+
     # sliders "sd" and "affinity" are in log10 scale
-    phase_clinical %>% 
-      dplyr::filter(mean_Kd <= 10 ^ input$filter_affinity) %>%
-      dplyr::filter(is.na(SD_aff) | SD_aff <= 10 ^ input$filter_sd) %>% 
-      dplyr::filter(n_measurement >= input$filter_measurement)
+    phase_clinical %>%
+      dplyr::filter(Kd_Q1 <= 10 ^ input$filter_affinity) %>%
+      dplyr::filter(n_measurement_kd >= input$filter_measurement)
   })
-  
+
   r_selection_chemprobes <- reactive({
     req(r_gene_known())
-    
-    data_selection_chemprobes %>% 
-      dplyr::mutate(mean_Kd = NA, SD_aff = NA, n_measurement = NA) %>%
+
+    data_selection_chemprobes %>%
+      dplyr::mutate(Kd_Q1 = NA, n_measurement_kd = NA, selectivity_class = "probe") %>%
       dplyr::filter(symbol %in% r_gene_known())
   })
-  
+
   r_selection_table <- reactive({
     req(
       r_gene_known(),
       r_selection_selectivity(),
       r_selection_clinical()
     )
-    
+
     target_cols <- c(
-      "gene_id", "molregno", "mean_Kd", "SD_aff", "n_measurement", "source"
+      "gene_id", "lspci_id", "KD_Q1", "SD_aff", "n_measurement_kd", "selectivity_class"
     )
-    
+
     this <- dplyr::bind_rows(
       r_selection_selectivity() %>% dplyr::select(!!target_cols),
       r_selection_clinical() %>% dplyr::select(!!target_cols)
     )
-    
+
     if (isTRUE("chem_probe" %in% input$filter_expert)) {
       req(r_selection_chemprobes())
-      
-      this <- this %>% 
+
+      this <- this %>%
         dplyr::bind_rows(
           r_selection_chemprobes() %>% dplyr::select(!!target_cols)
         )
     }
-    
+
     this
   })
-  
+
   r_table_entry <- reactive({
-    r_selection_table() %>% 
+    r_selection_table() %>%
       dplyr::inner_join(
-        data_cmpd_info %>% 
-          dplyr::select(molregno, chembl_id, pref_name, max_phase),
-        by = "molregno"
-      ) %>% 
+        data_cmpd_info %>%
+          dplyr::select(lspci_id, chembl_id, pref_name, max_phase),
+        by = "lspci_id"
+      ) %>%
       dplyr::inner_join(
         data_gene_info,
         by = "gene_id"
-      ) %>% 
-      dplyr::distinct() %>% 
+      ) %>%
+      dplyr::distinct() %>%
       dplyr::select(
         symbol, chembl_id,
-        pref_name, source, max_phase, mean_Kd, SD_aff, n_measurement,
+        pref_name, selectivity_class, max_phase, Kd_Q1, n_measurement_kd,
         gene_id, tax_id
-      ) %>% 
-      dplyr::mutate(
-        symbol = factor(symbol), 
-        chembl_id = factor(chembl_id), 
-        pref_name = factor(pref_name),
-        source = factor(source), 
-        gene_id = factor(gene_id), 
-        tax_id = factor(tax_id),
-        max_phase = as.integer(max_phase)
-      ) %>% 
+      ) %>%
+      dplyr::mutate_at(
+        vars(symbol, chembl_id, pref_name, source, gene_id, tax_id),
+        factor
+      ) %>%
+      dplyr::mutate_at(
+        vars(max_phase),
+        as.integer
+      ) %>%
       dplyr::mutate_at(           # rounds mean and SD to closest 0.1 if greater than 1.
-        vars(mean_Kd, SD_aff),    # if less than one, rounds to two significant digits.
+        vars(Kd_Q1),    # if less than one, rounds to two significant digits.
         ~ ifelse(. > 1, round(., 1), signif(., 2))
       ) %>%
       dplyr::rename(
-        `mean_Kd_(nM)` = mean_Kd, 
-        `SD_Kd_(nM)` = SD_aff, 
-        reason_included = source
+        `Kd_Q1_(nM)` = Kd_Q1,
+        reason_included = selectivity_class
       )
   })
-  
+
   r_table_compound <- reactive({
-    r_selection_table() %>% 
+    r_selection_table() %>%
       dplyr::inner_join(
-        data_cmpd_info %>% 
+        data_cmpd_info %>%
           dplyr::select(
-            molregno, chembl_id, pref_name, max_phase,
-            alt_names, inchi
+            lspci_id, chembl_id, pref_name, max_phase,
+            alt_names
           ),
-        by = "molregno"
-      ) %>% 
+        by = "lspci_id"
+      ) %>%
       dplyr::inner_join(
         data_gene_info,
         by = "gene_id"
-      ) %>% 
+      ) %>%
       dplyr::distinct() %>%
       dplyr::group_by(
-        molregno, chembl_id, pref_name, alt_names, inchi, max_phase
-      ) %>% 
+        lspci_id, chembl_id, pref_name, alt_names, max_phase
+      ) %>%
       dplyr::summarise(
-        sources = paste(symbol, "; ", source, collapse = ", ")
-      ) %>% 
-      dplyr::ungroup() %>% 
-      dplyr::mutate(
-        molregno = factor(molregno), 
-        chembl_id = factor(chembl_id),
-        pref_name = factor(pref_name),
-        max_phase = as.integer(max_phase)
-      ) %>% 
+        sources = paste(symbol, "; ", selectivity_class, collapse = ", ")
+      ) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate_at(
+        vars(lspci_id, chembl_id, pref_name),
+        factor
+      ) %>%
+      dplyr::mutate_at(
+        vars(max_phase),
+        as.integer
+      ) %>%
       dplyr::rename(
         reason_included = sources
       )
   })
-  
+
   r_tbl_data <- reactive({
     req(input$table_display)
-    
+
     .data <- if (input$table_display == "entry") {
       r_table_entry()
     } else if (input$table_display == "compound") {
       r_table_compound()
-    }    
+    }
   })
-  
+
   r_tbl <- reactive({
     view_type <- if (input$table_display == "entry") "target" else "compound"
     download_name <- create_download_filename(
       c("library", "small", "molecule", "suite"), c(view_type, "view")
     )
-    
-    .data <- r_tbl_data() %>% 
+
+    .data <- r_tbl_data() %>%
       dplyr::mutate(
         ` ` = NA_character_,
         chembl_id = lapply(
@@ -454,9 +439,9 @@ libraryServer <- function(input, output, session, load_example) {
           ),
           HTML
         ),
-      ) %>% 
+      ) %>%
       dplyr::select(` `, everything())
-    
+
     DT::datatable(
       data = .data,
       extensions = c("Buttons", "Select"),
@@ -468,11 +453,11 @@ libraryServer <- function(input, output, session, load_example) {
         buttons = list(
           list(extend = "copy"),
           list(
-            extend = "csv", 
+            extend = "csv",
             title = download_name
           ),
           list(
-            extend = "excel", 
+            extend = "excel",
             title = download_name
           ),
           list(
@@ -505,30 +490,30 @@ libraryServer <- function(input, output, session, load_example) {
       )
     )
   })
-  
+
   output$table_results <- DT::renderDataTable(
     r_tbl(),
     server = FALSE
   )
-  
+
   output$chembl_list <- renderUI({
     req(input$table_results_rows_selected)
-    
+
     tbl_selection <- r_tbl_data()[input$table_results_rows_selected, ]
     chembl_ids <- sort(unique(as.character(tbl_selection$chembl_id)))
-    
+
     chembl_first <- vector("logical", length(chembl_ids))
     chembl_first[1] <- TRUE
-    
+
     nav_link <- function(x, active) {
-      active <- if (active) " active" else "" 
-      glue("<a class='text-black nav-link{ active }' data-toggle='pill' 
+      active <- if (active) " active" else ""
+      glue("<a class='text-black nav-link{ active }' data-toggle='pill'
              data-target='#{ x }' role='tab'>{ x }</a>")
     }
-    
+
     tab_pane <- function(x, active) {
       active <- if (active) " show active" else ""
-      glue("<div class='tab-pane { active }' id='{ x }' 
+      glue("<div class='tab-pane { active }' id='{ x }'
              style='height: 750.5px'
              role='tabpanel'>
              <object data='https://www.ebi.ac.uk/chembl/embed/#compound_report_card/{ x }/name_and_classification'
@@ -536,7 +521,7 @@ libraryServer <- function(input, output, session, load_example) {
              </object>
            </div>")
     }
-    
+
     chembl_panes <- c(
       "<div class='row'>",
       "<div class='col-3'>",
@@ -551,8 +536,8 @@ libraryServer <- function(input, output, session, load_example) {
       "</div>",
       "</div>"
     )
-    
+
     HTML(glue_collapse(chembl_panes, "\n"))
   })
-  
+
 }
