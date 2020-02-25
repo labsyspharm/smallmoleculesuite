7ce0b04a8c10f9dd800b6119aef8c6292a96978e
Clemens Hug <clemens.hug@gmail.com>
2020-02-25 16:25:52 -0500

    Fix library module to work with new data

--- scripts/scripts_r/shiny_apps/smallmolculeapp/data/load.R
+++ scripts/scripts_r/shiny_apps/smallmolculeapp/data/load.R
@@ -12,12 +12,12 @@ for (col in c("toolscore", "ontarget_IC50_Q1", "offtarget_IC50_Q1", "IC50_diff",
   set(data_selection_selectivity, j = col, value = signif(data_selection_selectivity[[col]], digits = 2))
 }
 
-
 data_cmpd_info <- file.path(dir_data, "shiny_compounds_morgan_normal.fst") %>%
   fst::read_fst(as.data.table = TRUE)
 
 data_selection_chemprobes <- file.path(dir_data, "shiny_chemical_probes_morgan_normal.fst") %>%
-  fst::read_fst(as.data.table = TRUE)
+  fst::read_fst(as.data.table = TRUE) %>%
+  .[avg_rating == 4]
 
 data_gene_info <- file.path(dir_data, "shiny_targets_morgan_normal.fst") %>%
   fst::read_fst(as.data.table = TRUE)
